3187b56552e4ab2f1c1f3689b4c9de09c4c06e69
Mohammed Sayed <mohammedksayed@gmail.com>
2017-05-08 15:14:02 -0400

    Update Database

--- dbsetup.py
+++ dbsetup.py
@@ -1,6 +1,13 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+import os
+
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite://///Users/mksmasr/PycharmProjects/CashMe_Master/CashMeAppData.db'
+# dir = os.path.dirname(__file__)
+# databasefile = os.path.join(dir, 'CashMeAppData.db')
+
+db_path = os.path.join(os.path.dirname(__file__), 'CashMeAppData.db')
+db_uri = 'sqlite:///{}'.format(db_path)
+app.config['SQLALCHEMY_DATABASE_URI'] = db_uri
 db = SQLAlchemy(app)
